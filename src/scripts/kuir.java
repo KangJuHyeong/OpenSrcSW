dd9adba7b2ef241f1fd1fb0be246c9f6cd599339
KangJuHyeong <hjk7935@naver.com>
2022-04-17 12:59:42 +0900

    searcher

--- src/scripts/kuir.java
+++ src/scripts/kuir.java
@@ -1,18 +1,18 @@
 package scripts;
 
 import java.io.IOException;
-
 import javax.xml.transform.TransformerException;
-
 import scripts.makeCollection;
 import scripts.makeKeyword;
 import scripts.indexer;
 public class kuir {
-
+	 @SuppressWarnings({ "rawtypes", "unchecked", "nls" })
 	public static void main(String[] args) throws TransformerException, IOException, ClassNotFoundException {
 		
 		String command = args[0];   
 		String path = args[1];
+		String command2 =args[2];
+		String query=args[3];
 
 		if(command.equals("-c")) {
 			makeCollection collection = new makeCollection(path);
@@ -23,8 +23,12 @@ public class kuir {
 			keyword.convertXml();
 		}
 		else if(command.equals("-i")) {
-			indexer keyword = new indexer(path);
-			keyword.makepost();;
+			indexer index = new indexer(path);
+			index.makepost();
+		}
+		else if(command.equals("-s")) {
+			searcher search = new searcher(path,query);
+			search.srch();
 		}
 	}
 }
